a434d6b37148aaba82d2165bfa3e6f780ff370b7
nowfal-n <nowfal@metrictreelabs.com>
2018-12-14 10:57:24 +0530

    fixed untracked files

--- config/config.js
+++
@@ -1,9 +0,0 @@
-const config = {
-    mailChimp : {
-        instance : '', //Instance Id found at the end of the api key.
-        istUniqueId : '', //MailChimp Subscribers list Id.
-        apiKey : '' //MailChimp API key.
-    }
-}
-
-module.exports = config;
